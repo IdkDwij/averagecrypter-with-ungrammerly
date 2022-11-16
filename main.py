c373137e3474fba166c942875adae09d343501ac
Dwij <94942567+IdkDwij@users.noreply.github.com>
2022-11-16 14:23:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 import chars, sys
 
-fn = sys.argv
-fn.pop(0)
-m = fn[2]
-fn = " ".join(fn)
-with open(fn, 'r') as f:
+with open(sys.argv[2], 'r') as f:
     text = f.read()
 f.close()
 
 cry = ""
 uncry = ""
-if m == "e":
+if sys.argv[1] == "e":
     for x in text:
-        if x in chars.chardict:
-            print(chars.chardict[x])
+        if str(x) in chars.chardict:
             cry += str(chars.chardict[x])
-    with open(f"{fn}.cry", 'w') as f:
-        f.write(cry)
-    f.close()
-elif m == "d":
-    for x in cry:
-        if x in chars.unchar:
-            print(chars.unchar[x])
+    with open(f"{sys.argv[2]}.cry", 'w') as e:
+        e.write(cry)
+    e.close()
+elif sys.argv[1] == "d":
+    for x in text:
+        if str(x) in chars.unchar:
             uncry += str(chars.unchar[x])
-    with open(f"{fn}.uncry", 'w') as f:
-        f.write(uncry)
-    f.close()
+    with open(f"{sys.argv[2]}.uncry", 'w') as g:
+        g.write(uncry)
+    g.close()
